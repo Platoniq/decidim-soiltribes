01d5a753aae47f2fd389c0a540d18a1e624a5ad3
David Igón <david.igon@riseup.net>
2025-03-17 16:24:23 +0100

    remove schema load

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,6 @@ if ! bundle exec rails db:exists >/dev/null 2>&1; then
   echo "⚠️ Database does not exist. Creating the database..."
   bundle exec rails db:create
   echo "✅ Database created"
-  echo "Loading schema"
-  bundle exec rails db:schema:load
-
 else
   echo "✅ Database already exists"
 fi
