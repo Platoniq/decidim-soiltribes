d3677f0354bab1acce094829d288a5047b864816
David Igón <david.igon@riseup.net>
2025-03-31 17:36:49 +0200

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   mount Decidim::Core::Engine => '/'
-  authenticate :user ->(u) { u.admin? } do
+  authenticate :user, ->(u) { u.admin? } do
     mount Sidekiq::Web => "/sidekiq"
   end
 end
