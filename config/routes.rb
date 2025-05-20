4f38b2082d81cc67667e0af2a398e5815e62770e
Harrison Hassig <harrisonhassig@gmail.com>
2025-05-20 09:38:05 +0200

    add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   mount Decidim::Core::Engine => "/"
-  authenticate :user, ->(u) { u.admin? } do
-    mount Sidekiq::Web => "/sidekiq"
-  end
+  # authenticate :user, ->(u) { u.admin? } do
+  #   mount Sidekiq::Web => "/sidekiq"
+  # end
 end
