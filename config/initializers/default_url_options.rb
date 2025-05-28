5a61c94650eb6cf381b233c31b892156b86c70ed
Harrison Hassig <harrisonhassig@gmail.com>
2025-05-28 13:17:15 +0200

    set default url options in initializer

---
+++ config/initializers/default_url_options.rb
@@ -0,0 +1,8 @@
+host = ENV.fetch("APP_HOST", "soiltribes.platoniq.net")
+
+Rails.application.routes.default_url_options[:host] = host
+
+ActionMailer::Base.default_url_options = {
+  host: host,
+  protocol: "https"
+}
