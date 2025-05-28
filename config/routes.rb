3cea924693bb7fca993085cf8b4c71658d7c5041
Harrison Hassig <harrisonhassig@gmail.com>
2025-05-28 09:13:06 +0200

    routes for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   mount Decidim::Core::Engine => "/"
-  # authenticate :user, ->(u) { u.admin? } do
-  #   mount Sidekiq::Web => "/sidekiq"
-  # end
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 end
