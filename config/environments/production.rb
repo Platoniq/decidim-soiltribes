a80bf77b70b0aec345dfc39df92f42251162d409
Harrison Hassig <harrisonhassig@gmail.com>
2025-05-28 13:03:58 +0200

    set smtp host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,8 @@ Rails.application.configure do
     :password => Rails.application.secrets.smtp_password,
     :domain => Rails.application.secrets.smtp_domain,
     :enable_starttls_auto => Rails.application.secrets.smtp_starttls_auto,
-    :openssl_verify_mode => "none"
+    :openssl_verify_mode => "none",
+    :host => "soiltribes"
   }
 
   # Use a different logger for distributed setups.
