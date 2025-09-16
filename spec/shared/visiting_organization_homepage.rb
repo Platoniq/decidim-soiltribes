2d22435321d3c996985f3b3c55bd6d1895b86426
Harrison Hassig <harrisonhassig@gmail.com>
2025-09-16 15:38:43 +0200

    Rubocop

--- spec/shared/visiting_organization_homepage.rb
+++ spec/shared/visiting_organization_homepage.rb
@@ -2,7 +2,7 @@
 
 RSpec.shared_context "when visiting organization homepage" do
   let(:organization) { create(:organization) }
-  
+
   before do
     switch_to_host(organization.host)
     visit decidim.root_path
